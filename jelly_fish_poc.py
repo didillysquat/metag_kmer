48a607155c8087ae28280560b77d9290be7581cc
didillysquat <didillysquat@gmail.com>
2021-11-18 09:21:37 +0100

    plot using 2 kmers

--- jelly_fish_poc.py
+++ jelly_fish_poc.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import numpy as np
 
 class JellyCount:   
     def __init__(self):
@@ -95,11 +96,11 @@ class JellyCount:
         self.normalised_df_5000 =  self.df_var_sorted.iloc[:,:5000]
         
         fig, ax_arr = plt.subplots(ncols=3, nrows=5, figsize=(10,10))
-        self._compute_pca(df=self.normalised_df_20, ax_arr=ax_arr[0])
-        self._compute_pca(df=self.normalised_df_50, ax_arr=ax_arr[1])
-        self._compute_pca(df=self.normalised_df_100, ax_arr=ax_arr[2])
-        self._compute_pca(df=self.normalised_df_1000, ax_arr=ax_arr[3])
-        self._compute_pca(df=self.normalised_df_5000, ax_arr=ax_arr[4])
+        self._compute_lda(df=self.normalised_df_20, ax_arr=ax_arr[0])
+        self._compute_lda(df=self.normalised_df_50, ax_arr=ax_arr[1])
+        self._compute_lda(df=self.normalised_df_100, ax_arr=ax_arr[2])
+        self._compute_lda(df=self.normalised_df_1000, ax_arr=ax_arr[3])
+        self._compute_lda(df=self.normalised_df_5000, ax_arr=ax_arr[4])
         plt.tight_layout()
         plt.savefig("LDA_POC.png", dpi=600)
     
@@ -132,7 +133,7 @@ class JellyCount:
         ax_arr[2].scatter(bc_pcoa.samples["PC1"], bc_pcoa.samples["PC4"],c=colours)
       
 
-    def _compute_pca(self, df, ax_arr):
+    def _compute_lda(self, df, ax_arr):
         # Let's try linear discriminant analysis
         clf = LinearDiscriminantAnalysis()
         x = df.values
@@ -153,7 +154,7 @@ class JellyCount:
         ax_arr[2].set_xlabel("PC1")
         ax_arr[2].set_ylabel("PC4")
         
-    def investigate_LDA_kmers(self, top_kmer_n=1000):
+    def investigate_LDA_kmers(self, top_kmer_n=50):
         # The LDA results are pretty amazing. They show us that there is the information in the stacks to differentiate between the groups
         # However, this is supervised so not so great for us.
         # We need to use this data to work out which the good kmers are and which the bad kmers are and then look
@@ -182,6 +183,7 @@ class JellyCount:
         ax.set_ylabel("Minimum relative abundance of kmer across all samples")
         plt.tight_layout()
         plt.savefig(f"investigate top {top_kmer_n} variant kmer abundances")
+        plt.close()
 
         # We can see that it is indeed the kmers that are found in high abundance in samples but 0 abundance in others
         # That are coming up as the high variance kmers, and I guess this makes perfect sense.
@@ -189,6 +191,71 @@ class JellyCount:
         # Couple of things to go from here, the number 0.0004 seems like a good upper cutoff for the minimum max abundance
         # and 0 is good for the minimum abundance.
         # I want to know what this 0.0004 cutoff acutaly translates to interms of how abundant the most abundant kmers are.
+        print(f"quantile across samples normalised abundance {self.normalised_df.quantile(axis=1, q=0.99)}")
+        # It turns out that 0.0004 actually equates to kmers that are extremely abundant (within the top 1%)
+        # i.e. .99 quartile so we are only considering extremely abundant kmers.
+        # I'd like to know for each sample how many we're left with if we filter by abve quantil .99
+        # and that they must be abscent in other samples
+        # Let's try that here
+        # First work out those kmers that are abscent in at least 1 of the samples
+        # We want to work out the quantiles only from the kmers with counts
+        quantiles_99_dict = {}
+        for sample in self.normalised_df.index:
+            ser = self.normalised_df.loc[sample]
+            ser = ser[ser > 0]
+            quantiles_99_dict[sample] = ser.quantile(.98)
+        quantiles_99 = pd.Series(quantiles_99_dict)
+        # quantiles_99 = self.normalised_df.quantile(axis=1, q=0.99).values
+        selected = self.normalised_df.ge(quantiles_99, axis=0).sum(axis=1)
+        counts = (self.normalised_df > 0).sum(axis=1)
+        props_kept = selected/counts
+
+
+        # I also want to look at the kmers of the top 50 that best correlate to the PC that have been found by the LDA
+        # and see if we can tease out some characters that way
+        clf = LinearDiscriminantAnalysis()
+        x = self.normalised_top_var_df.values
+        # Standardizing the features
+        x = StandardScaler().fit_transform(x)
+        svds = self.meta_info_df.loc[self.normalised_top_var_df.index,"svd"].values
+        lda_w_transform = clf.fit_transform(x, svds)
+        fig, axarr = plt.subplots(ncols=4, nrows=1, figsize=(15,5))
+        # I want to plot up a couple of historams of the scaling
+        axarr[0].hist(abs(clf.scalings_[:,0]), bins=100)
+        axarr[1].hist(abs(clf.scalings_[:,1]), bins=100)
+        axarr[2].hist(abs(clf.scalings_[:,2]), bins=100)
+        axarr[3].hist(abs(clf.scalings_[:,3]), bins=100)
+        # We want to inspect the scalings
+        plt.savefig("LDA_sclaling_hists.png", dpi=600)
+        plt.close()
+
+        # This is super cool. It shows that there are only a very few kmers that have strong scalings for the LDA component
+        # Let's see which the Kmers are and see if we can recreate the LDA ordiation using these kmers, then
+        # we can plot them up on the scatrer to find their properties
+
+        # There is 1 kmer for each of the PCs that has a large scaling
+        # I will isolate these 4 kmers
+        best_scaling_kmers = []
+        for i in range(4):
+            ind = np.where(clf.scalings_[:,i] == np.amax(clf.scalings_[:,i]))
+            best_scaling_kmers.append(self.normalised_top_var_df.columns.values[ind][0])
+
+        two_kmers = list(set(best_scaling_kmers))
+        # Let's see if we can plot up just the two kmers as x and y and see where we get to
+        fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(5,5))
+        ax.scatter(self.normalised_df[two_kmers[0]], self.normalised_df[two_kmers[1]], c=[self.svd_color_dict[self.meta_info_df.at[_,'svd']] for _ in self.normalised_df.index])
+
+        # best_scaling_kmers = set()
+        # for i in range(4):
+        #     scaling = pd.Series(abs(clf.scalings_[:,i]), index=self.normalised_top_var_df.columns)
+        #     best_scaling_kmers.update(scaling[scaling > 0.15].index.values)
+        # foo = "bar"
+        
+        foo = "bar"
+
+        # Don't forget to check to see at what depth we'll need to be sequencing to pickup the meaningful kmers
+        plt.close()
+
         foo = "bar"
 
 JellyCount().investigate_LDA_kmers()
